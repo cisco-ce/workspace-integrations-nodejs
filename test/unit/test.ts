5b7fb7fd7ba76764cc71cb3ee38827044fe7d844
Tore Bjolseth <tbjolset@cisco.com>
2023-05-30 14:25:50 +0200

    add tests for event/status notifications

--- test/unit/test.ts
+++ test/unit/test.ts
@@ -71,6 +71,7 @@ describe('Connecting integration', () => {
 
 describe('Device API', () => {
   const devices = new DevicesImpl(http);
+
   it('can fetch devices with correct api', async () => {
     await devices.getDevices();
     expect(lastHttpCall).toEqual(testdata.http.getDevices);
@@ -85,17 +86,39 @@ describe('Device API', () => {
 describe('xAPI', () => {
   const xapi = new XapiImpl(http);
   const deviceId = '12345';
+
   it('can query status with correct api', async () => {
     await xapi.status.get(deviceId, 'RoomAnalytics.*');
     expect(lastHttpCall).toEqual(testdata.http.getXapiStatus);
   });
+
   it('can invoke a command', async () => {
     await xapi.command(deviceId, 'Dial', { Number: 'chuck@cisco.com' });
     expect(lastHttpCall).toEqual(testdata.http.invokeXapiCommand);
   });
+
   it('can invoke a command with multi-line', async () => {
     await xapi.command(deviceId, 'CustomStatus Set', { Email: 'santa@cisco.com' }, 'On vacation');
     expect(lastHttpCall).toEqual(testdata.http.invokeXapiCommandWithMultiline);
   });
 
+  it('can route status notifications', (done) => {
+    xapi.status.on('RoomAnalytics.PeopleCount.Current', (deviceId, path, value) => {
+      // console.log('got', deviceId, path, value);
+      expect(deviceId).toBe('1234');
+      expect(path).toBe('RoomAnalytics.PeopleCount.Current');
+      expect(value).toBe(2);
+      done();
+    });
+    xapi.processNotification(testdata.notifications.status);
+  });
+
+  it('can route events', (done) => {
+    xapi.event.on('UserInterface.Message.Prompt.Response', (deviceId, path) => {
+      expect(deviceId).toBe('1234');
+      expect(path).toBe('UserInterface.Message.Prompt.Response');
+      done();
+    });
+    xapi.processNotification(testdata.notifications.event);
+  });
 });
